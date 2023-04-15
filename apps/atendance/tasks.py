aff02be73dea81acc5842646ae26aac97c6a773c
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2023-04-15 11:43:39 +0500

    fix: update celery task attendance

--- apps/atendance/tasks.py
+++ apps/atendance/tasks.py
@@ -1,5 +1,25 @@
+from datetime import datetime
+
 from .models import Attendance
+from apps.staf.models import Staff
 from .api_endpoints.attendanse.AttendenseCreate import AttendanceSerializer
 
 from celery import shared_task
+
+
+@shared_task
+def create_attendance(staff_id):
+    staff = Staff.objects.get(id=staff_id)
+    if staff not in Attendance.objects.filter(date=datetime.today()):
+        status = Attendance.objects.create(staff=staff, status="kelmagan", date=datetime.today())
+        return AttendanceSerializer(status).data
+
+
+
+
+
+
+
+
+
 
